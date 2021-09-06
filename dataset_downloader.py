172c9f7f70691368d72a330d48bd665b8ac79e4b
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-09-06 14:57:13 -0400

    added a string to control destination folder

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -4,3 +4,7 @@ import shutil
 
 #the base format for links that download the tiff files
 base_link = "https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/1/TIFF/{northsouthdegree}{eastwestdegree}/USGS_1_{northsouthdegree}{eastwestdegree}.tif"
+
+#the folder directory where our images will go
+folder = "rawdata/"
+

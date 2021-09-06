bc5cb81086e4504119aa202e2c1ed54e79280911
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-09-06 14:52:17 -0400

    added a formattable string which can provide valid links to image downloads

---
+++ dataset_downloader.py
@@ -0,0 +1,2 @@
+#the base format for links that download the tiff files
+base_link = "https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/1/TIFF/{northsouthdegree}{eastwestdegree}/USGS_1_{northsouthdegree}{eastwestdegree}.tif"

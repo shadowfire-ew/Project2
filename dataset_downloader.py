4436be43e1d61606f2ca8eb28b68ef0466d940c5
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-09-06 14:58:26 -0400

    another import to reduce downloading of images we already have

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -1,6 +1,7 @@
 # imports
 import requests
 import shutil
+from os.path import exists
 
 #the base format for links that download the tiff files
 base_link = "https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/1/TIFF/{northsouthdegree}{eastwestdegree}/USGS_1_{northsouthdegree}{eastwestdegree}.tif"

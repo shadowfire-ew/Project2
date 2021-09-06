ebaac2cdba25502b6be4eb59ef89fb0340db7a40
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-09-06 15:23:17 -0400

    changed the base link

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -4,7 +4,7 @@ import shutil
 from os.path import exists
 
 #the base format for links that download the tiff files
-base_link = "https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/1/TIFF/{northsouthdegree}{eastwestdegree}/USGS_1_{northsouthdegree}{eastwestdegree}.tif"
+base_link = "https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/13/TIFF/n{northdegree}{eastwest}{ewdegree}/USGS_13_n{northdegree}{eastwest}{ewdegree}.tif"
 
 #the folder directory where our images will go
 folder = "rawdata/"

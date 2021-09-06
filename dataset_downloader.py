c5519f750cb8ec190f80101249a3e5e8de3be635
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-09-06 15:57:50 -0400

    implementing automatic downloader
    with help from here:  https://towardsdatascience.com/how-to-download-an-image-using-python-38a75cfa21c

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -14,3 +14,24 @@ names = []
 with open("names.txt",'r') as f:
     names = f.read().split('\n')
 
+for name in names:
+    destination = folder+name+".tif"
+    if exists(destination):
+        print(destination,"already exists")
+    else:
+        url = base_link.format(fname=name)
+        print("retrieving",name,"from",url)
+        req = requests.get(url,stream=True)
+
+        if req.status_code == 200:
+            print("saving to",destination)
+            req.raw.decode_content = True
+
+            with open(destination,'wb') as f:
+                shutil.copyfileobj(req.raw,f)
+            
+            print("finished saving to",destination)
+        
+        else:
+            print("could not retrieve from",destination)
+

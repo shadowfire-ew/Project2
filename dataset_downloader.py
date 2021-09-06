4ab6d86407c5f1275aad26a4c0dfa5e32730f32e
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-09-06 14:54:39 -0400

    added imports which will be used

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -1,2 +1,6 @@
+# imports
+import requests
+import shutil
+
 #the base format for links that download the tiff files
 base_link = "https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/1/TIFF/{northsouthdegree}{eastwestdegree}/USGS_1_{northsouthdegree}{eastwestdegree}.tif"

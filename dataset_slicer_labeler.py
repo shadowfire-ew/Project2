689e2719fa6438e2c0525e0286aacffbc018729d
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-09-06 16:55:08 -0400

    Update dataset_slicer_labeler.py

--- dataset_slicer_labeler.py
+++ dataset_slicer_labeler.py
@@ -1,9 +1,11 @@
 from PIL import Image
 
+import numpy as np
+
 # disabling the limit temporarily
 Image.MAX_IMAGE_PIXELS = None
 
-im = Image.open("rawdata/n19w156.tif")
+# this is where our loop will go
 
 # re-enabling the limit just in case
 Image.MAX_IMAGE_PIXELS = 89478485

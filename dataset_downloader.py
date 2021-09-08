207d0dbaf770065962656559abc9ca18b19edc3c
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-09-08 10:26:10 -0400

    slight modification to encapsulate loop in main

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -9,29 +9,30 @@ base_link = "https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/13/TIFF/{
 #the folder directory where our images will go
 folder = "rawdata/"
 
-names = []
+if __name__ == "__main__":
+    names = []
 
-with open("names.txt",'r') as f:
-    names = f.read().split('\n')
+    with open("names.txt",'r') as f:
+        names = f.read().split('\n')
 
-for name in names:
-    destination = folder+name+".tif"
-    if exists(destination):
-        print(destination,"already exists")
-    else:
-        url = base_link.format(fname=name)
-        print("retrieving",name,"from",url)
-        req = requests.get(url,stream=True)
-
-        if req.status_code == 200:
-            print("saving to",destination)
-            req.raw.decode_content = True
-
-            with open(destination,'wb') as f:
-                shutil.copyfileobj(req.raw,f)
-            
-            print("finished saving to",destination)
-        
+    for name in names:
+        destination = folder+name+".tif"
+        if exists(destination):
+            print(destination,"already exists")
         else:
-            print("could not retrieve from",destination)
+            url = base_link.format(fname=name)
+            print("retrieving",name,"from",url)
+            req = requests.get(url,stream=True)
 
+            if req.status_code == 200:
+                print("saving to",destination)
+                req.raw.decode_content = True
+
+                with open(destination,'wb') as f:
+                    shutil.copyfileobj(req.raw,f)
+                
+                print("finished saving to",destination)
+            
+            else:
+                print("could not retrieve from",destination)
+    input()

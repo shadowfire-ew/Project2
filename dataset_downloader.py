6e37a89b8a9c7a33a5678141e89e54766fd56217
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-09-08 11:16:32 -0400

    getting our missing files

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -2,9 +2,10 @@
 import requests
 import shutil
 from os.path import exists
+from collections import defaultdict
 
 #the base format for links that download the tiff files
-base_link = "https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/13/TIFF/{fname}/USGS_13_{fname}.tif"
+base_link = "https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/13/TIFF/{fname}/USGS_13_{fname}{time}.tif"
 
 #the folder directory where our images will go
 folder = "rawdata/"
@@ -14,17 +15,25 @@ if __name__ == "__main__":
 
     misses=[]
 
+    catches=defaultdict(lambda:'')
+
     with open("names.txt",'r') as f:
         names = f.read().split('\n')
+
+    with open("catches.txt",'r') as f:
+        temp = f.read().split('\n')
+        for part in temp:
+            a = part.split(' ')
+            catches[a[0]]='_'+a[1]
 
     for name in names:
         destination = folder+name+".tif"
         if exists(destination):
-            # print(destination,"already exists")
+            print(destination,"already exists")
             pass
         else:
-            url = base_link.format(fname=name)
-            # print("retrieving",name,"from",url)
+            url = base_link.format(fname=name,time=catches[name])
+            print("retrieving",name,"from",url)
             req = requests.get(url,stream=True)
 
             if req.status_code == 200:
@@ -34,10 +43,10 @@ if __name__ == "__main__":
                 with open(destination,'wb') as f:
                     shutil.copyfileobj(req.raw,f)
                 
-                # print("finished saving to",destination)
+                print("finished saving to",destination)
             
             else:
-                # print("could not retrieve from",url)
+                print("could not retrieve from",url)
                 misses.append(name)
     
     with open("misses.txt",'w') as f:

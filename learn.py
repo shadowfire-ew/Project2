e72832d6defae9a49709ffa715a32a35cc823de8
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-09-14 14:32:05 -0400

    Create learn.py

---
+++ learn.py
@@ -0,0 +1,5 @@
+"""
+this file will hold all the nescessary functions for learning
+will need to create .py files for each type of learning schema
+i think that i will be able to make this scalable
+"""

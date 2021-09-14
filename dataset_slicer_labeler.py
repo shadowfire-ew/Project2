bad9ada7a6963e3a61b8f6a9732cb00dedb927e8
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-09-14 14:32:01 -0400

    preparing a class to handle slicing and labeling

--- dataset_slicer_labeler.py
+++ dataset_slicer_labeler.py
@@ -5,7 +5,59 @@ import numpy as np
 # disabling the limit temporarily
 Image.MAX_IMAGE_PIXELS = None
 
-# this is where our loop will go
+class ImageSet:
+    """
+    this class will hold onto a set of images
+
+    """
+    def __init__(self,sliceSize):
+        self._images = [[None,None,None],
+                        [None,None,None],
+                        [None,None,None]]
+        self._names = [ [None,None,None],
+                        [None,None,None],
+                        [None,None,None]]
+        self._xOffset = 0
+        self._yOffset = 0
+        self._sliceSize = sliceSize
+        self._labels=[]
+    
+    def loadGroup(self,name):
+        """
+        takes the name and loads it and the images next to it spatially
+        If the image name is already in our names list, we will shift their references
+        this would recenter our focused name and put Nones in the unloaded images' spaces
+        if the name is not in our names set, it will reload the entire array of images
+        """
+    
+    def getNextSlice(self):
+        """
+        returns a slice and label of our loaded images
+        the slice will be an array of the size decided upon init
+        the label will be one of two things:
+            - the label for the center point if it is within one of our label polygons
+            - None for anything outside
+        after getting the slice, it will itterate the offset(s)
+        it will avoid corners/edges with missing peices
+        all slices will be centered within the cCenter image
+        """
+    
+    def addLabel(self,name,polygon):
+        """
+        add a labeling polygon to the labels dictionary
+        this will be used for labeling points
+        enforcing strict tying for both parameters:
+            - name is str
+            - polygon must be a polygon-like
+        """
+        if (type(name) is str) and (type(polygon) is list):
+            # TODO: look into a dedicated polygon class which will determine point containment
+            self._labels[name]=polygon
+        else:
+            raise TypeError
+
+if __name__ == "__main__":
+    m=ImageSet()
 
 # re-enabling the limit just in case
 Image.MAX_IMAGE_PIXELS = 89478485

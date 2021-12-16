52b61f614132f03708b0f5a6b48b0ef11cd28723
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-12-16 14:03:47 -0500

    hypothesis

--- neuralnet.py
+++ neuralnet.py
@@ -27,13 +27,37 @@ class NeuralNetwork:
             # because im using 1XN array/matrix/list for input
             self._thetas.append(np.random.rand(sizes[ind]+1,sizes[ind+1]))
     
+    def Hypothesis(self,inarray):
+        """
+        takes an input numpy array and transforms it through the neural network
+        """
+        inlen = len(inarray.shape)
+        if inlen == 1:
+            inlen = inarray[0]
+        elif inlen == 2:
+            inlen = inarray[1]
+        else:
+            inlen=-1
+        if inarray.shape[0] != self._inputnum:
+            print("Wrong input shape, expecting m by {a}".format(self._inputnum))
+        # get starting values
+        outarray=inarray
+        for theta in self._thetas:
+            # add bias
+            outarray = np.concatenate(([1],outarray))
+            # linear transform into new dimensions
+            outarray = np.matmul(outarray,theta)
+            # apply sigmoid
+            outarray = sigmoid(outarray)
+        # return final array
+        return(outarray)
 
 if __name__ == "__main__":
     # test NN constructor
     testNN = NeuralNetwork((3,2,1))
     for arr in testNN._thetas:
         print(arr.shape)
-    """--------
+    """#------------
     # test sigmoid
     x = np.linspace(-10,10,100)
     z = sigmoid(x)
@@ -41,4 +65,10 @@ if __name__ == "__main__":
     plt.xlabel("X")
     plt.ylabel("Sigmoid(X)")
     plt.show()
-    #"""
+    #"""#------------
+    """#------------
+    # test hypothesis
+    x = np.array([0.6,0.9,0.1])
+    y = testNN.Hypothesis(x)
+    print(y)
+    #"""#------------

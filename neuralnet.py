45f9f44089e15b25b65d0327a0b565634959d7d9
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-12-16 13:33:50 -0500

    constructor

---
+++ neuralnet.py
@@ -0,0 +1,30 @@
+import numpy as np
+class NeuralNetwork:
+    """
+    a class that handles the neural network features and functions
+    """
+    def __init__(self,sizes=(1,1)):
+        """
+        takes an input luplte of sizes
+        needs to be at least len 2
+        index[0] = input number
+        index[-1] = output number
+        """
+        if type(sizes) is not tuple:
+            print("please input a tuple")
+            raise Exception
+        if len(sizes) < 2:
+            print("please input")
+        self._inputnum = sizes[0]
+        self._thetas=[]
+        for ind in range((len(sizes)-1)):
+            # shaped specifically like this
+            # because im using 1XN array/matrix/list for input
+            self._thetas.append(np.random.rand(sizes[ind]+1,sizes[ind+1]))
+    
+
+if __name__ == "__main__":
+    # test NN constructor
+    testNN = NeuralNetwork((3,2,1))
+    for arr in testNN._thetas:
+        print(arr.shape)

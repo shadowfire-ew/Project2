78e0c9095e89bd24ad4ef8c7b1401c405a157593
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-12-28 22:18:27 -0500

    fixed early exit checks

--- learn.py
+++ learn.py
@@ -92,8 +92,11 @@ def Teach(fname,slice_size,slice_step,alpha,epochs,lam,labels,hidden_layers=()):
         costs.append(cost_left+cost_right)
         # early exit on cost increasing or negligible
         if i > 0:
-            if costs[i]-costs[i-1]>=0:
+            if costs[i] >= costs[i-1]:
                 print("Cost increasing or not changing. skipping descent and exiting...")
+                break
+            if costs[i] == 0:
+                print("Cost has reached global minimum. skipping descent and exiting...")
                 break
         # get derivative
         derivative = net.Derive(len(sumparts),lam)
